dfc44ed3bb8abae326d152c19d8aba75fc16d062
pablocru <pablocru@outlook.com>
2026-04-19 21:15:07 +0200

    feat(mappers): add GitHub user profile mapper

---
+++ src/mappers/github.mapper.ts
@@ -0,0 +1,21 @@
+import type { GitHubUserProfile, GitHubUserRaw } from "../types/github.type";
+import { toUrlOrNull } from "../utils/url.util";
+
+export function mapGitHubUser(raw: GitHubUserRaw): GitHubUserProfile {
+  const fullname = raw.name;
+
+  return {
+    username: raw.login,
+    fullname,
+    displayName: fullname ?? raw.login,
+    location: raw.location ?? null,
+    bio: raw.bio ?? null,
+    personalSite: toUrlOrNull(raw.blog),
+    pictureUrl: new URL(raw.avatar_url),
+    pageUrl: new URL(raw.html_url),
+    repoCount: raw.public_repos,
+    gistsCount: raw.public_gists,
+    createdAt: new Date(raw.created_at),
+    updatedAt: new Date(raw.updated_at),
+  };
+}
