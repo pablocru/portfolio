cc11cea324631a93b954ecaf4dc681d3fa90ab99
pablocru <pablocru@outlook.com>
2026-04-21 00:00:07 +0200

    feat(services): handle errors with Octokit Request Error library

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,3 +1,4 @@
+import { RequestError } from "@octokit/request-error";
 import { Octokit } from "@octokit/rest";
 import { mapGitHubUser } from "../mappers/github.mapper";
 import type { GitHubUserProfile } from "../types/github.type";
@@ -8,10 +9,21 @@ export class GitHubService {
   constructor(private username: string) {}
 
   async getUserProfile(): Promise<GitHubUserProfile> {
-    const { data } = await this.octokit.rest.users.getByUsername({
-      username: this.username,
-    });
+    try {
+      const { data } = await this.octokit.rest.users.getByUsername({
+        username: this.username,
+      });
 
-    return mapGitHubUser(data);
+      return mapGitHubUser(data);
+    } catch (error) {
+      const errorMessage: string =
+        error instanceof RequestError
+          ? `GitHub API error (${error.status}): ${error.message}`
+          : error instanceof Error
+            ? error.message
+            : "Unknown error";
+
+      throw new Error(errorMessage, { cause: error });
+    }
   }
 }
